4efcb926be7a2a7138b9c034ebbaf9acf6c3a0e1
Jacob Hummer <jcbhmr@outlook.com>
2023-08-22 16:22:59 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ namespace=${INPUT_COLLECTION#*/}
 registry=${INPUT_COLLECTION%/$namespace}
 devcontainer features publish -r "$registry" -n "$namespace" "$INPUT_PATH"
 
-if [[ $INPUT_TAG == true ]]; then
+if [[ $INPUT_UPDATE_TAGS == true ]]; then
   for id in "$INPUT_PATH"/*; do
     id=$(basename "$id")
     if [[ ! -d "$INPUT_PATH/$id" ]]; then
